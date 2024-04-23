8e1f957b2a58405ea322cc212d40fc3cceadca16
devajji-anitha <devajjianitha1103@gmail.com>
2024-04-23 14:00:04 +0530

    hello script

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=Anitha
-PERSON2=Three
+PERSON1=mylove
+PERSON2=andme
 echo "$PERSON1:: hello $PERSON2,how are you?"
 echo "$PERSON2:: hi $PERSON1,iam fine .what about you?"
 echo "$PERSON1:: iam good,how was your work?"
