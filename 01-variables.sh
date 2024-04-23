e0f884e38a670d0404c9f5548f43f1c2cb53e23c
devajji-anitha <devajjianitha1103@gmail.com>
2024-04-23 12:38:54 +0530

    hello script

---
+++ 01-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ani:: hello hari,how are you?"
+echo "hari:: hi ani,iam fine.what about you?"
+echo "ani:: iam good,how was your work?"
+echo "hari:: its goind good,thinking to upgrade new skills"
