36d6840da1d1059191ee470386cd87950be3e562
devajji-anitha <devajjianitha1103@gmail.com>
2024-04-24 12:39:57 +0530

    shell-script

--- 08-installation.sh
+++ 08-installation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
- sudo dnf install mysql -y
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root access."
+    else 
+        echo "you are super user."
+    fi      
+ 
+ dnf install mysql -y
          
